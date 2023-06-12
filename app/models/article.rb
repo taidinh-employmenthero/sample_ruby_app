ddd018cc8ae7ad8bc18f048a343eb6116227c0ac
taidinh-employmenthero <tai.dinhthanh@employmenthero.com>
2023-06-12 14:00:20 +0700

    Add CRUD operation with Comment entity

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  include Visible
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 10}
+  has_many :comments, dependent: :destroy
 end
